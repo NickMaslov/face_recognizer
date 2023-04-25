980ea399fff2bb32b52701bb9eb014e0e69ca065
Nick Maslov <kategorychnost@gmail.com>
2023-04-24 20:56:11 -0400

    refactoring with command line arguments

--- detector.py
+++ detector.py
@@ -3,11 +3,30 @@ import face_recognition
 import pickle
 from collections import Counter
 from PIL import Image, ImageDraw
+import argparse
 
 
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
 BOUNDING_BOX_COLOR = "blue"
 TEXT_COLOR = "white"
+
+parser = argparse.ArgumentParser(description="Recognize faces in an image")
+parser.add_argument("--train", action="store_true", help="Train on input data")
+parser.add_argument("--validate", action="store_true", help="Validate trained model")
+parser.add_argument(
+    "--test", action="store_true", help="Test the model with an unknown image"
+)
+parser.add_argument(
+    "-m",
+    action="store",
+    default="hog",
+    choices=["hog", "cnn"],
+    help="Which model to use for training: hog (CPU), cnn (GPU)",
+)
+parser.add_argument(
+    "-f", action="store", default="hog", help="Path to an image with an unknown face"
+)
+args = parser.parse_args()
 
 Path("training").mkdir(exist_ok=True)
 Path("output").mkdir(exist_ok=True)
@@ -23,19 +42,17 @@ def encode_known_faces(
         name = filepath.parent.name
         image = face_recognition.load_image_file(filepath)
 
-    face_locations = face_recognition.face_locations(image, model=model)
-    face_encodings = face_recognition.face_encodings(image, face_locations)
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
 
-    for encoding in face_encodings:
-        names.append(name)
-        encodings.append(encoding)
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
 
+    # print(names)
     name_encodings = {"names": names, "encodings": encodings}
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
-
-
-encode_known_faces()
 
 
 def recognize_faces(
@@ -62,7 +79,6 @@ def recognize_faces(
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
-        # Removed print(name, bounding_box)
         _display_face(draw, bounding_box, name)
         print(name, bounding_box)
 
@@ -97,4 +113,21 @@ def _display_face(draw, bounding_box, name):
     )
 
 
-recognize_faces("unknown.jpg")
+def validate(model: str = "hog"):
+    for filepath in Path("validation").rglob("*"):
+        if filepath.is_file():
+            recognize_faces(image_location=str(filepath.absolute()), model=model)
+
+
+encode_known_faces()
+# recognize_faces("unknown.jpg")
+# validate()
+
+
+if __name__ == "__main__":
+    if args.train:
+        encode_known_faces(model=args.m)
+    if args.validate:
+        validate(model=args.m)
+    if args.test:
+        recognize_faces(image_location=args.f, model=args.m)
